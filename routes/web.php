0a20de062a420202272ffd6a63461984edf89fc8
ozonerik <ozonerik@gmail.com>
2026-05-10 20:17:31 +0700

    tambahkan route admin optimize

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,11 @@
 
 use App\Models\JenisSurat;
 use App\Models\Sekolah;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Route;
+use Filament\Notifications\Notification;
 
 Route::get('/', function () {
     $sekolah = Sekolah::first() ?? new Sekolah();
@@ -28,3 +32,47 @@ Route::get('/templates/{jenisSurat}/download', function (JenisSurat $jenisSurat)
 
     abort(404);
 })->middleware('auth')->name('templates.download');
+
+Route::get('/admin/optimize', function () {
+    /** @var User|null $user */
+    $user = Auth::user();
+
+    try {
+
+        try {
+            Artisan::call('filament:optimize');
+        } catch (Throwable $throwable) {
+            $message = $throwable->getMessage();
+
+            $isMissingIconsNamespace = str_contains($message, '"icons" namespace');
+            $isFilamentOptimizeNotDefined = str_contains($message, 'Command "filament:optimize" is not defined');
+
+            if (! $isMissingIconsNamespace && ! $isFilamentOptimizeNotDefined) {
+                throw $throwable;
+            }
+        }
+
+        Artisan::call('storage:link');
+        Artisan::call('optimize:clear');
+        Artisan::call('permission:cache-reset');
+
+        Notification::make()
+            ->title('Optimize selesai.')
+            ->success()
+            ->send();
+
+        return redirect('/admin');
+    } catch (Throwable $throwable) {
+        Log::error('Optimize route command failed.', [
+            'exception' => $throwable,
+        ]);
+
+        Notification::make()
+            ->title('Optimize error')
+            ->body($throwable->getMessage())
+            ->danger()
+            ->send();
+
+        return redirect('/admin');
+    }
+})->middleware('auth')->name('optimize.route');
